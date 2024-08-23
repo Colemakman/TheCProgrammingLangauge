a60e3940fad41c3d244c634dc458283c20e97887
Alfie Baxter <34726358+Colemakman@users.noreply.github.com>
2024-08-23 01:42:48 +0100

    Exercise 1-17

---
+++ chapters/ch1/longer_than_80.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+#define MAXLINE 1000 
+
+
+int gtline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+
+int main() {
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+
+	while ((len = gtline(line, MAXLINE)) > 0) {
+		if (len > max) {
+			max = len;
+			copy(longest, line);
+		}
+		if (len >= 80)
+			printf("%s", line);
+	}
+
+	if (max > 0) {
+		printf("Longest line: %s\n", longest);
+		printf("Length: %d\n", max);
+	}
+
+	return 0;
+}
+
+/* getline: read a line into s, return length */
+
+int gtline(char s[], int lim) {
+	
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c=getchar()) != EOF && c != '\n'; i++)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+
+void copy(char to[], char from[]) {
+
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		i++;
+}
